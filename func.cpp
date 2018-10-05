81f1a507698f0a1e5691a3224d0ce4611d63f4eb
keimoriyama <keischwiiz@gmail.com>
2018-10-05 20:23:23 +0900

    大体動作した

--- func.cpp
+++ func.cpp
@@ -4,19 +4,23 @@ void lift_in_right()
 {
 	if (reccon[RSY] > highest)
 	{
-		dtsp = dtmid;
+		dtsp = -dtmid;
 		mcsp = mcmid;
-		if(!spflag)motordrive(r_in, dtsp);
-		else if(spflag)motordrive(r_in,mcsp);
-		printf("right outside up\n");
+		if (!spflag)
+			motordrive(r_in, dtsp);
+		else if (spflag)
+			motordrive(r_in, mcsp);
+		printf("right inside up\n");
 	}
 	else if (reccon[RSY] < lowest)
 	{
-		dtsp = -dtmid;
+		dtsp = dtmid;
 		mcsp = mcmid;
-		if(!spflag)motordrive(r_in, dtsp);
-		else if(spflag)motordrive(r_in,mcsp);
-		printf("right outside down\n");
+		if (!spflag)
+			motordrive(r_in, dtsp);
+		else if (spflag)
+			motordrive(r_in, mcsp);
+		printf("right inside down\n");
 	}
 	else
 	{
@@ -25,22 +29,27 @@ void lift_in_right()
 	}
 }
 
-void lift_in_left(){
+void lift_in_left()
+{
 	if (reccon[LSY] > highest)
 	{
-		dtsp = dtmid;
+		dtsp = -dtmid;
 		mcsp = mcmid;
-		if(!spflag)motordrive(l_in, dtsp);
-		else if(spflag)motordrive(l_in,mcsp);
-		printf("left outside up\n");
+		if (!spflag)
+			motordrive(l_in, dtsp);
+		else if (spflag)
+			motordrive(l_in, mcsp);
+		printf("left inside up\n");
 	}
 	else if (reccon[LSY] < lowest)
 	{
-		dtsp = -dtmid;
+		dtsp = dtmid;
 		mcsp = -mcmid;
-		if(!spflag)motordrive(l_in, dtsp);
-		else if(spflag)motordrive(l_in,mcsp);
-		printf("left outside down\n");
+		if (!spflag)
+			motordrive(l_in, dtsp);
+		else if (spflag)
+			motordrive(l_in, mcsp);
+		printf("left inside down\n");
 	}
 	else
 	{
@@ -48,21 +57,6 @@ void lift_in_left(){
 		printf("left inside stop\n");
 	}
 }
-/*
-void dt_mc(int sel ,float value){
-	if((value < 1 && value > -1) && spflag == 0){
-		dtsp = value;
-		motordrive(sel,dtsp);
-		printf("dt\n");
-	}else if( spflag == 1){
-		mcsp = value;
-		motordrive(sel,mcsp);
-		printf("mc\n");
-	}else{
-		printf("error\n");
-		motordrive(sel,0);
-	}
-}*/
 
 void config()
 {
@@ -76,24 +70,30 @@ void config()
 		{
 			if (reccon[DY] < lowest)
 			{ //LSY_上昇
-				dtsp = dtmid;
-				mcsp = -mcmid;
-				if(!spflag)motordrive(l_out, dtsp);
-				else if(spflag)motordrive(l_out, mcsp);
+				dtsp = -dtmid;
+				mcsp = mcmid;
+				if (!spflag)
+					motordrive(l_out, dtsp);
+				else if (spflag)
+					motordrive(l_out, mcsp);
 				printf("left outside up\n");
 			}
 			else if (reccon[DY] > highest)
 			{ //LSY＿下降
-				dtsp = -dtmid;
+				dtsp = dtmid;
 				mcsp = mcmid;
-				if(!spflag)motordrive(l_out, dtsp);
-				else if(spflag)motordrive(l_out, mcsp);
+				if (!spflag)
+					motordrive(l_out, dtsp);
+				else if (spflag)
+					motordrive(l_out, mcsp);
 				printf("left outside down\n");
 			}
 			else
 			{ //停止
-				if(!spflag)motordrive(l_out, 0);
-				else if(spflag)motordrive(l_out, 0);
+				if (!spflag)
+					motordrive(l_out, 0);
+				else if (spflag)
+					motordrive(l_out, 0);
 				printf("left outside stop\n");
 			}
 		}
@@ -111,14 +111,38 @@ void config()
 		break;
 
 	case LB:
+		if (reccon[BUTTON] & (1 << 4))
+		{
+			motordrive(pen, -0.3);
+			printf("penduram start\n");
+		}
+		else
+		{
+			motordrive(pen, 0);
+			printf("penduram stop\n");
+		}
+		break;
 	case RB:
+		if (reccon[BUTTON] & (1 << 5))
+		{
+			motordrive(pen, 0.3);
+			printf("config penduram\n");
+		}
+		else
+		{
+			motordrive(pen, 0);
+			printf("penduram stop\n");
+		}
+		break;
 	case Y: //右外上昇
-		dtsp = dtmid;
+		dtsp = -dtmid;
 		mcsp = mcmid;
 		if (reccon[BUTTON] & (1 << 3))
 		{
-			if(!spflag)motordrive(r_out, dtsp);
-			else if(spflag)motordrive(r_out,mcsp);
+			if (!spflag)
+				motordrive(r_out, dtsp);
+			else if (spflag)
+				motordrive(r_out, mcsp);
 			printf("right outside up\n");
 		}
 		else
@@ -126,82 +150,83 @@ void config()
 			motordrive(r_out, 0);
 			printf("right outside stop\n");
 		}
-	break;
+		break;
 
-case X: //
+	case X: //
 
-case A: //右外下降
-	dtsp = -dtmid;
-	mcsp = -mcmid;
-	if (reccon[BUTTON] & (1 << 0))
-	{
-		if(!spflag)motordrive(r_out, dtsp);
-		else if(spflag)motordrive(r_out,mcsp);
-		printf("right outside down\n");
-	}
-	else
-	{
-		motordrive(r_out, 0);
-		printf("right outside stop\n");
-	}
+	case A: //右外下降
+		dtsp = dtmid;
+		mcsp = -mcmid;
+		if (reccon[BUTTON] & (1 << 0))
+		{
+			if (!spflag)
+				motordrive(r_out, dtsp);
+			else if (spflag)
+				motordrive(r_out, mcsp);
+			printf("right outside down\n");
+		}
+		else
+		{
+			motordrive(r_out, 0);
+			printf("right outside stop\n");
+		}
 
+	case B:		//ダッシュ
+	case START: //
+		if (reccon[BUTTON] & (1 << 7))
+		{
+			if (st_status == 0)
+			{
+				st_status = 1;
+				printf("----------------------\n-----setting mode-----\n----------------------\n");
+			}
+			else if (st_status == 1)
+			{
+				st_status = 0;
+				printf("----------------------\n-------move mode------\n----------------------\n");
+			}
+		}
+		else
+		{
+		}
+		break;
 
-case B:		//ダッシュ
-case START: //
-if (reccon[BUTTON] & (1 << 7))
-{
-	if (st_status == 0)
-	{
-		st_status = 1;
-		printf("----------------------\n-----setting mode-----\n----------------------\n");
-	}
-	else if (st_status == 1)
-	{
-		st_status = 0;
-		printf("----------------------\n-------move mode------\n----------------------\n");
-	}
-}
-else
-{
-}
-break;
+	case MIDBUT:
+		if (reccon[BUTTON] & (1 << 8))
+		{
+			if (spflag == 0)
+			{
+				spflag = 1;
+				printf("mc\n");
+			}
+			else if (spflag == 1)
+			{
+				spflag = 0;
+				printf("dt\n");
+			}
+			else
+			{
+			}
+		}
+		else
+		{
+		}
+		break;
 
-case MIDBUT:
-if (reccon[BUTTON] & (1 << 8))
-{
-	if (spflag == 0)
-	{
-		spflag = 1;
-		printf("mc\n");
-	}
-	else if (spflag == 1)
-	{
-		spflag = 0;
-		printf("dt\n");
-	}
-	else
-	{
-	}
-}
-else
-{
-}
-break;
+	case BACK: //リセット
+		if (reccon[BUTTON] & (1 << 6))
+		{
+			serialPrintf(srid, "sel 0\r");
+			serialPrintf(srid, "reboot\r");
+			printf("all stop and reboot\n");
+		}
+		else
+		{
+			printf("completed\n");
+		}
 
-case BACK: //リセット
-if (reccon[BUTTON] & (1 << 6))
-{
-	serialPrintf(srid, "sel 0\r");
-	serialPrintf(srid, "reboot\r");
-	printf("all stop and reboot\n");
-}
-else
-{
-	printf("completed\n");
-}
-
-break;
-}
+		break;
+	}
 }
 
 void move()
@@ -268,69 +293,62 @@ void move()
 		break;
 
 	case Y: //射出スイッ
-			dtsp = -dtmid;
-			printf("%4.2f  ", dtsp);
-			if (reccon[BUTTON] & (1 << 3))
-			{
-				motordrive(throwwww,dtsp);
-				printf("throwww\n");
-			}
-			else
-			{
-				motordrive(throwwww,0);
-				printf("throwww\n");
-			}
+		dtsp = -dtmid;
+		printf("%4.2f  ", dtsp);
+		if (reccon[BUTTON] & (1 << 3))
+		{
+			motordrive(throwwww, dtsp);
+			printf("penduram start\n");
+		}
+		else
+		{
+			motordrive(throwwww, 0);
+			printf("penduram stop\n");
+		}
 		break;
 
 	case X: //
-			dtsp = 0.5;
-			printf("%4.2f  ", dtsp);
-			if (reccon[BUTTON] & (1 << 2))
-			{
-				motordrive(pen,-dtsp);
-			}
-			else
-			{
-				motordrive(pen,0);
-			}
+		dtsp = -0.3;
+		printf("%4.2f  ", dtsp);
+		if (reccon[BUTTON] & (1 << 2))
+		{
+			motordrive(pen, dtsp);
+			printf("throwwww\n");
+		}
+		else
+		{
+			motordrive(pen, 0);
+			printf("throwww stop\n");
+		}
 		break;
 
 	case A: //スロー
-			dtsp = dtmid;
-			//printf("%4.2f  ", dtsp);
-			if (reccon[BUTTON] & (1 << 0))
-			{
-				lift(dtsp);
-				/*motordrive(r_in,dtsp);
-				motordrive(r_out,dtsp);
-				motordrive(l_in,dtsp);
-				motordrive(l_out,dtsp);*/
-				printf("lift move up\n");
-			}
-			else
-			{
-				lift_stop();
-				/*motordrive(r_in,0);
-				motordrive(r_out,0);
-				motordrive(l_in,0);
-				motordrive(l_out,0);*/
-				printf("lift stop\n");
-			}
+		dtsp = dtmid;
+		//printf("%4.2f  ", dtsp);
+		if (reccon[BUTTON] & (1 << 0))
+		{
+			lift(dtsp);
+			printf("lift move up\n");
+		}
+		else
+		{
+			lift_stop();
+			printf("lift stop\n");
+		}
 		break;
 
 	case B: //ダッシュ
-			dtsp = -dtmid;
-			//printf("%4.2f  ", dtsp);
-			if (reccon[BUTTON] & (1 << 1))
-			{
-				lift(dtsp);
-				printf("lift move down\n");
-			}
-			else
-			{
-				lift_stop();
-				printf("lift stop\n");
-			}
+		dtsp = -dtmid;
+		if (reccon[BUTTON] & (1 << 1))
+		{
+			lift(dtsp);
+			printf("lift move down\n");
+		}
+		else
+		{
+			lift_stop();
+			printf("lift stop\n");
+		}
 		break;
 
 	case START: //
